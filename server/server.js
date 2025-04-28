f57caa0f1f5115d8c938b3f00cdb34746e8af3f3
Aradhya Srivastava <aradhya610@gmail.com>
2025-04-29 01:38:30 +0530

    made changes

--- server/server.js
+++ server/server.js
@@ -57,14 +57,24 @@ app.use('/api/bids', bidRoutes);
 app.use('/api/uploads', uploadsRoutes);
 
 // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('../client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+//   });
+// } 
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('../client/build'));
+  // Correct path for serving frontend build
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
-} else {
+}
+
+else {
   // In development mode, handle API calls and ensure 404s are sent to React Router
   app.use((req, res, next) => {
     if (req.url.startsWith('/api')) {
